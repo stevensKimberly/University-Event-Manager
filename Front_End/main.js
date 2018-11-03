4878370a407813bcc0f4d2986965a3bf33477506
Jonathan Cotto <cottojonathan@ymail.com>
2018-11-03 15:42:22 -0400

    Add files via upload

--- Front_End/main.js
+++ Front_End/main.js
@@ -53,52 +53,48 @@ $(function () {
                 var count = 0;
 
                 //for each event make a card
-                $.each(events, function(i, event){
-                    var html = "<div class='card h-100 mb-4'>  <div class='card-body'> <h5 class='card-title'>"+ event.eventTitle +"</h5>" +
-                     "<h6 class='card-subtitle mb-2 text-muted'>" + event.rsoHosting + "</h6>" +
-                      "<p class='card-text'>" + event.eventDescription + "</p>" +
-                      "<a class='eventMoreInfo' data-eventID=' "+ event.eventID +" href='#' class='card-link'>More Info</a>" +
-                      "<a class='eventUnfollow' data-eventID='"+event.eventID + "href='#' class='card-link'>Unfollow</a></div></div>";
-                    
-                      //append the html inside the eventcarddeck
-                      $('#eventCardDeck').append(html);
+                $.each(events, function (i, event) {
+                    var html = "<div class='card h-100 mb-4'>  <div class='card-body'> <h5 class='card-title'>" + event.eventTitle + "</h5>" +
+                        "<h6 class='card-subtitle mb-2 text-muted'>" + event.rsoHosting + "</h6>" +
+                        "<p class='card-text'>" + event.eventDescription + "</p>" +
+                        "<a class='eventMoreInfo' data-eventID=' " + event.eventID + " href='#' class='card-link'>More Info</a>" +
+                        "<a class='eventUnfollow' data-eventID='" + event.eventID + "href='#' class='card-link'>Unfollow</a></div></div>";
 
-                     count++; //increment count
-                      //for the responisve design
-                      if(i === 2)
-                      {
+                    //append the html inside the eventcarddeck
+                    $('#eventCardDeck').append(html);
+
+                    count++; //increment count
+                    //for the responisve design
+                    if (i === 2) {
                         //every two cards sm
                         $('#eventCardDeck').append('<div class="w-100 d-none d-sm-block d-md-none">');
-                      }
+                    }
 
-                      if(i === 3)
-                      {
+                    if (i === 3) {
                         //every three cards md
                         $('#eventCardDeck').append('<div class="w-100 d-none d-md-block d-lg-none"');
 
-                      }
+                    }
 
 
-                      if(i === 4)
-                      {
-                          //every four cards lg
-                          $('#eventCardDeck').append('<div class="w-100 d-none d-lg-block d-xl-none">');
-                      }
+                    if (i === 4) {
+                        //every four cards lg
+                        $('#eventCardDeck').append('<div class="w-100 d-none d-lg-block d-xl-none">');
+                    }
 
 
-                      if(i === 5)
-                      {
-                          //every five cards XL
-                          $('#eventCardDeck').append('<div class="w-100 d-none d-xl-block">');
-                      }
+                    if (i === 5) {
+                        //every five cards XL
+                        $('#eventCardDeck').append('<div class="w-100 d-none d-xl-block">');
+                    }
                 });
-                
 
-                
+
+
             },
             error: function () {
                 //detach out all child elements;
-                $('#eventCardDeck').children().detach();
+                //$('#eventCardDeck').children().detach();
                 alert("Server Failed To Retrieve Events");
             }
         });
@@ -107,8 +103,8 @@ $(function () {
 
 
 
-     //get all rsos for my rsos tab
-     $('#myRSOs-tab').on('click', function () {
+    //get all rsos for my rsos tab
+    $('#myRSOs-tab').on('click', function () {
         //send to backend 
         $.ajax({
             type: "GET",
@@ -117,95 +113,100 @@ $(function () {
             success: function (rsos) {
                 //detach out all child elements;
                 $('#rsoCardDeck').children().detach();
-                
+
                 var count = 0; //count the amount of cards
                 //for each event make a card
-                $.each(rsos, function(i, rso){
+                $.each(rsos, function (i, rso) {
 
-                    var html = "<div class='card h-100 mb-4'>  <div class='card-body'> <h5 class='card-title'>"+ rso.rsoTitle +"</h5>" +
-                     "<h6 class='card-subtitle mb-2 text-muted'>" + rso.rsoType + "</h6>" +
-                      "<p class='card-text'>" + rso.rsoDescription + "</p>" +
-                      "<a data-eventID=' "+ rso.rsoID +" href='#' class='card-link'>More Info</a>" +
-                      "<a data-eventID='"+rso.rsoID + "href='#' class='card-link'>Unfollow</a></div></div>";
-                    
-                      //append the html inside the eventcarddeck
-                      $('#rsoCardDeck').append(html);
+                    var html = "<div class='card h-100 mb-4'>  <div class='card-body'> <h5 class='card-title'>" + rso.rsoTitle + "</h5>" +
+                        "<h6 class='card-subtitle mb-2 text-muted'>" + rso.rsoType + "</h6>" +
+                        "<p class='card-text'>" + rso.rsoDescription + "</p>" +
+                        "<a data-eventID=' " + rso.rsoID + " href='#' class='card-link'>More Info</a>" +
+                        "<a data-eventID='" + rso.rsoID + "href='#' class='card-link'>Unfollow</a></div></div>";
 
-                      count++; //increment card
+                    //append the html inside the eventcarddeck
+                    $('#rsoCardDeck').append(html);
 
-                      //for the responisve design
-                      if(count  === 2)
-                      {
+                    count++; //increment card
+
+                    //for the responisve design
+                    if (count === 2) {
                         //every two cards sm
                         $('#rsoCardDeck').append('<div class="w-100 d-none d-sm-block d-md-none">');
-                      }
+                    }
 
-                      if(count === 3)
-                      {
+                    if (count === 3) {
                         //every three cards md
                         $('#rsoCardDeck').append('<div class="w-100 d-none d-md-block d-lg-none"');
 
-                      }
+                    }
 
 
-                      if(count === 4)
-                      {
-                          //every four cards lg
-                          $('#rsoCardDeck').append('<div class="w-100 d-none d-lg-block d-xl-none">');
-                      }
+                    if (count === 4) {
+                        //every four cards lg
+                        $('#rsoCardDeck').append('<div class="w-100 d-none d-lg-block d-xl-none">');
+                    }
 
 
-                      if(count === 5)
-                      {
-                          //every five cards XL
-                          $('#rsoCardDeck').append('<div class="w-100 d-none d-xl-block">');
-                      }
+                    if (count === 5) {
+                        //every five cards XL
+                        $('#rsoCardDeck').append('<div class="w-100 d-none d-xl-block">');
+                    }
 
-                      
+
                 });
-                
 
-                
             },
             error: function () {
                 //detach out all child elements;
-                $('#rsoCardDeck').children().detach();
+                // $('#rsoCardDeck').children().detach();
                 alert("Server Failed To Retrieve RSOs");
             }
         });
     });
 
 
+    //load eventpage
+    //send eventID 
+    //receive event info
+    $('.eventMoreInfo').on('click', function () {
+        //get event id
+        var eventID = $(this).attr('data-eventID');
 
-    $('.eventMoreInfo').on('click', function(){
-    //get event id
-      var eventID =  $(this).attr('data-eventID');
+        //create object
+        var obj = {
+            "eventID": eventID
+        };
 
-    //create object
-    var obj = {
-        "eventID": eventID
-    };
 
-    var jsonEventID = JSON.stringify(obj);
+        var jsonEventID = JSON.stringify(obj);
 
-    $.ajax({
-        type: "GET",
-        url: "moreInfoEvent.php",
-        data: jsonEventID,
-        success: function(){
 
-        },
-        error: function(){
+        //send event id
+        $.ajax({
+            type: "GET",
+            url: "moreInfoEvent.php",
+            data: jsonEventID,
+            success: function () {
+                //goto event page
+                window.location.href = "eventPage.html";
 
-        }
+
+                //update eventpage info
+
+
+            },
+            error: function () {
+
+            }
+        });
+
+
     });
 
-    
-    });
 
-
-    $('#createRSOBtn').on('click', function(){
-
+    $('#createRSOBtn').on('click', function () {
+        console.log("in hererereer");
         //get rso info
         var rsoTitle = $('#rsoTitle').text();
         var rsoDescription = $('#rsoDescription').text();
@@ -224,14 +225,14 @@ $(function () {
         var jsonRSO = JSON.stringify(obj);
 
         $.ajax({
-            type: "GET",
+            type: "POST",
             url: "createRSO.php",
             data: jsonRSO,
-            sucess: function(){
+            sucess: function () {
 
             },
-            error: function(){
-
+            error: function () {
+                console.log("failedddd");
             }
         });
     });
@@ -239,5 +240,5 @@ $(function () {
 
 
 
-});
+});//end of jquery function
 
