e12bf076572dbb3e2efd095ea279f523fec3ec0b
stevensKimberly <44677440+stevensKimberly@users.noreply.github.com>
2018-11-06 22:31:42 -0500

    Update connect.php

--- Back_End/connect.php
+++ Back_End/connect.php
@@ -3,7 +3,7 @@
 	$server = 'dbschedule.cm0a6cdoupkr.us-east-1.rds.amazonaws.com';
 	$user = 'Ashley';
 	$password = 'Sunshine1916';
-	$dbName = 'scheduler';
+	$dbName = 'Scheduler';
 
 	$connect = mysqli_connect($server, $user,$password,$dbName) or die("Error: Cannot connect");
 ?>
