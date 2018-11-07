2db391de1191761dcd0f90e40f4bd683cf4e0edb
stevensKimberly <44677440+stevensKimberly@users.noreply.github.com>
2018-11-06 22:52:12 -0500

    Update loginUser.php

--- Back_End/loginUser.php
+++ Back_End/loginUser.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html>
 <?php
+	require_once 'session.php';
+
 	// Connection to database.
 	require('connect.php');
 	
@@ -7,21 +8,42 @@
 	$user = $_POST['username'];
 	$pass = $_POST['password'];
 	
-	// Check that a row for this username and password exists.
-	$sqlChkUser = "SELECT userId
-				   FROM users
-				   WHERE userName = '$user' AND password = '$pass'";
-				   
-	$result = mysqli_query($connect, $sqlChkUser);
-	$numResult = mysqli_num_rows($result);
-
-	// Return error message if the information is incorrect.
-	if($numResult != 1)
-	{
-		exit("Invalid login information");
+	// Prepared statement to see if there is a user with a matching username and password.
+	$sql = "SELECT userId 
+			FROM users 
+			WHERE userName = ? AND password = ?";
+			
+	// Have the database parse the SQL.
+	$stmt = mysqli_prepare($connect, $sql);
+	if (!$stmt) {
+		// could not compile the query (problem with query)
+		exit(mysqli_error($connect));
 	}
 	
-	// $id holds the user id of the user to load the proper events later, other pages will require this information.
-	$row = $result->fetch_assoc();
-	$id = (int) $row['userId'];
-?>
+	// Populate the placeholders.
+	$stmt->bind_param('ss', $user, $pass);
+	
+	// Execute the query with placeholders populated.
+	if (!$stmt->execute()) {
+		// could not execute the query
+		echo 'Prepared statement failed!!!\n';
+		exit(mysqli_error($connect));
+	}
+	
+	// If that user exists, then create a session with their userID.
+	$matchedUser = false;
+	$stmt->bind_result($userId);
+	if($stmt->fetch())
+	{
+		$_SESSION['logged-in-user'] = $userId;
+		logUserIn($_SESSION['logged-in-user']);
+		$matchedUser = true;
+	}
+	
+	// Clean up.
+	$stmt->free_result();
+	$stmt->close();
+	
+	if (!$matchedUser) {
+		exit('no user found');
+	}
